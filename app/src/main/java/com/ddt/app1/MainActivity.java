287c52442f81652dc042dfb13b18504bc66a4b74
David Mojíca <davidmojicav@gmail.com>
2024-02-06 09:19:24 -0500

    Cm2

--- app/src/main/java/com/ddt/app1/MainActivity.java
+++ app/src/main/java/com/ddt/app1/MainActivity.java
@@ -4,19 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    //Variable declaration
+    public boolean ban = true;
+    protected int n = 12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
     }
-
 }
 
-namespace nvm {
-    
-        }
+//Objects
+
+
+
