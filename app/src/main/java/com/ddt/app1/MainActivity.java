0da181cae4c1d95f1b8952f40b525beae34f6e10
David Mojíca <102682432+DavidMojicaDev@users.noreply.github.com>
2024-02-08 09:21:28 -0500

    Added event listeners

--- app/src/main/java/com/ddt/app1/MainActivity.java
+++ app/src/main/java/com/ddt/app1/MainActivity.java
@@ -10,15 +10,38 @@ public class MainActivity extends AppCompatActivity {
     //Variable declaration
     public boolean ban = true;
     protected int n = 12;
+    protected string textChanged1 = "Suspicius soup";
+    protected string textChanged2 = "Suspicius soup";
 
+
+
+    //There, we can do a 'spiritual DOM with the interface elements'
+    private TextView userName;
+    private Button btn1;
+    private Button btn2;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //GetElementById
+        userName = (TextView) findViewById(R.id.username);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+
+        
+        btn1.setOnCLickListener(new View.setOnCLickListener(){
+
+            @Override
+            public void OnClick(View view){
+                userName.setText(textChanged1);
+            }
+        
+        });
     }
 }
 
-//Objects
 
 
 
